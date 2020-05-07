b7fdd1500b082fad18981094a628b375cf0c61ee
Tomtfm <61930724+Tomtfm@users.noreply.github.com>
2020-05-07 11:29:49 +0800

    mini

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -28,39 +28,29 @@ Page({
     // tabScrollTop: 0
   },
   onLoad: function (options) {
-    // 1.获取转入的iid
     this.setData({
       iid: options.iid
     })
-
-    // 2.请求商品详情数据
+    
     this._getDetailData()
 
-    // 3.请求商品推荐的数据
     this._getRecommends()
   },
-  // ============== 网络请求区 ===================
   _getDetailData() {
     getDetail(this.data.iid).then(res => {
       const data = res.data.result;
       // console.log(data);
       
-      // 1.取出顶部的图片
       const topImages = data.itemInfo.topImages;
       
-      // 2.创建BaseInfo对象
       const baseInfo = new GoodsBaseInfo(data.itemInfo, data.columns, data.shopInfo.services)
 
-      // 3.创建shopInfo对象
       const shopInfo = new ShopInfo(data.shopInfo)
 
-      // 4.获取detailInfo信息
       const detailInfo = data.detailInfo
 
-      // 5.创建 ParamInfo对象
       const paramInfo = new ParamInfo(data.itemParams.info, data.itemParams.rule)
 
-      // 6.获取评论信息
       let commentInfo = {}
       if(data.rate && data.rate.cRate > 0) {
         commentInfo = data.rate.list[0]
@@ -87,7 +77,6 @@ Page({
   },
   // 点击加入购物车事件
   onAddCart() {
-    // 1.获取商品对象
     const obj = {}
     obj.iid = this.data.iid;
     obj.imageURL = this.data.topImages[0];
@@ -95,10 +84,8 @@ Page({
     obj.desc = this.data.baseInfo.desc;
     obj.price = this.data.baseInfo.realPrice;
     
-    // 2.加入到购物车列表
     app.addToCart(obj)
 
-    // 3.加入成功提示
     wx.showToast({
       title: '成功加入购物车'
     })
