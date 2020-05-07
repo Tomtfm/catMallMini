99deb3b497ebd80fd81e12a7311f973bf22b904f
Tomtfm <61930724+Tomtfm@users.noreply.github.com>
2020-05-07 11:26:55 +0800

    mini

--- pages/category/childCpns/t-menu/t-menu.js
+++ pages/category/childCpns/t-menu/t-menu.js
@@ -10,13 +10,10 @@ Component({
   },
   methods: {
     onItemClick(e) {
-      // 1.改变当前的 currentIndex
       const currentIndex = e.currentTarget.dataset.index;
       this.setData({
         currentIndex
       })
-
-      // 2.将最新的 currentIndex 传递到分类页面
       this.triggerEvent('menuclick', {currentIndex}, {})
     }
   }
