777479a7e8d795b86c1a4f874920dd167ac0efc0
Tomtfm <61930724+Tomtfm@users.noreply.github.com>
2020-05-07 11:22:13 +0800

    mini

--- pages/cart/childCpns/cart-list-item/cart-list-item.js
+++ pages/cart/childCpns/cart-list-item/cart-list-item.js
@@ -16,15 +16,11 @@ Component({
   },
   methods: {
     onCheckClick(e) {
-      // 1.查找到对应的商品
       const goods = app.globalData.cartList.find(item => item.iid == this.properties.goods.iid)
       goods.checked = !goods.checked
 
-      // 2.获取当前商品的inex
       const index = e.currentTarget.dataset.index
 
-      // 3.回调
-      // console.log(index);
       app.changeGoodsState(index, goods)
     }
   }
